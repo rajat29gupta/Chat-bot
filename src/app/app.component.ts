7ec3cb2b1b43fbd8c6f437338e4d9d21aa39dbac
rajat <rajat29gupta@gmail.com>
2019-07-22 12:57:03 +0530

    new temp added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+   openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+   closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
 }
